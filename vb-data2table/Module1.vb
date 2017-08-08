49d71b32a5dc598719b3c676ff30cf51eecc159d
Brian Hodges <bhodges@PMIDSD.com>
2017-08-08 11:16:41 -0400

    initial code commit

---
+++ vb-data2table/Module1.vb
@@ -0,0 +1,63 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim c1 As New Country("USA", "350 million")
+        Dim c2 As New Country("China", "1.4 billion")
+        Dim c3 As New Country("Russia", "145 million")
+        Dim countries = New Country() {c1, c2, c3}
+
+        Console.WriteLine("DUMP:")
+        Console.WriteLine("[")
+        For Each country As Country In countries
+            Console.WriteLine(vbTab + "{ ""Name"":""" + country.Name + """, ""Population"":""" + country.Population + """}")
+        Next
+        Console.WriteLine("]")
+
+        Console.WriteLine()
+        Console.WriteLine("SANITIZED:")
+        For Each country As Country In countries
+            Console.WriteLine("{:Name => " + country.Name + ", :Population => " + country.Population + "}")
+        Next
+
+        Console.WriteLine()
+        Console.WriteLine("TABLE:")
+        Dim header As String = "Countries | Population"
+        Console.WriteLine(header)
+        Console.WriteLine(StrDup(Len(header), "-"))
+        For Each country As Country In countries
+            Console.WriteLine(country.Name + StrDup((Len("Countries ") - Len(country.Name)), " ") + "|" + country.Population)
+        Next
+
+        Console.WriteLine()
+        Console.WriteLine("Press any key to exit:")
+        Console.ReadKey()
+    End Sub
+
+    Public Class Country
+        Private title, pop As String
+
+        Public Sub New(ByVal Name As String, Population As String)
+            Me.Name = Name
+            Me.Population = Population
+        End Sub
+
+        Public Property Name() As String
+            Get
+                Return title
+            End Get
+            Set(ByVal Value As String)
+                title = Value
+            End Set
+        End Property
+
+        Public Property Population() As String
+            Get
+                Return pop
+            End Get
+            Set(ByVal Value As String)
+                pop = Value
+            End Set
+        End Property
+    End Class
+
+End Module
---
+++ vb-data2table/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("vb-data2table")> 
+<Assembly: AssemblyDescription("")> 
+<Assembly: AssemblyCompany("")> 
+<Assembly: AssemblyProduct("vb-data2table")> 
+<Assembly: AssemblyCopyright("Copyright ©  2017")> 
+<Assembly: AssemblyTrademark("")> 
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("15250ded-0994-45eb-b4d3-669d7b60f184")> 
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")> 
+<Assembly: AssemblyFileVersion("1.0.0.0")> 
